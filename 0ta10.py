42021ae6838068cc80bba21d2420990ba32cfe97
Ghalandar73 <amirmahdisafarpour@gmail.com>
2025-11-30 23:51:06 +0330

    Update 0ta10.py

--- 0ta10.py
+++ 0ta10.py
@@ -1,5 +1,4 @@
-Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+
 >>> def up(x):
 ...     print(f'up({x}+1)')
 ...     if(x<=9):
@@ -7,3 +6,4 @@ Enter "help" below or click "Help" above for more information.
 ...     else:
 ...         return 0
 ...         
+
